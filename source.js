c9d3f2a134d6f20488f120bf588a375b72533daf
brianSites <54866029+brianSites@users.noreply.github.com>
2019-09-05 20:34:26 -0400

    Update source.js

--- source.js
+++ source.js
@@ -252,7 +252,6 @@ function playlistPanel (data) {
 		var description = item.snippet.description.substring(0, 150) + "...";
 
 		$("#playlist-panel").append(`
-
 			<article class="playlist-video" data-id="${id}">
 			
 			<img src="${thumbnail}" class="playlist-video-thumbnail">
@@ -266,176 +265,100 @@ function playlistPanel (data) {
 			</div>
 			
 			</article>
-
 		`);
 	});
 }
 
 function controlPanel () {
 	$("#control-panel").html(`
-
 		<div>
-
 			<label class="control-type">Player Controls</label>
-
 			<hr>
-
 			<div id="link-converter" class="button-container">
-
 				<div>
-
 					<input id="link" class="input" placeholder="Link Goes Here">
-
 					<div id="link-button" class="button" onclick="convert();">Submit Link</div>
-
 				</div>
-
 				<div>
-
 					<input id="id" class="input" placeholder="Id Goes Here">
-
 					<div id="id-button" class="button" onclick="start();">Submit ID</div>
-
 				</div>
-
 			</div>
 			
 			<br>
-
 		</div>
-
 		<div>
-
 			<label class="control-type">Play Controls</label>
-
 			<hr>
-
 			<div class="button-container">
-
 				<div id="play-button" class="button" onclick="play();">Play</div>
-
 				<div id="pause-button" class="button" onclick="pause();">Pause</div>
-
 				<div id="stop-button" class="button" onclick="stop();">Stop</div>
-
 			</div>
 			
 			<br>
-
 		</div>
-
 		<div>
-
 			<label class="control-type">Playlist Controls</label>
-
 			<hr>
-
 			<div class="input-button-container">
-
 				<div id="playprevious-button" class="button" onclick="playPrevious();">Play Previous</div>
-
 				<div id="playnext-button" class="button" onclick="playNext();">Play Next</div>
-
 				<input id="index" class="input" placeholder="Index In Playlist">
-
 				<div id="playindex-button" class="button" onclick="doPlayIndex();">Play Number</div>
-
 			</div>
 			
 			<br>
-
 		</div>
-
 		<div>
-
 			<label class="control-type">Mute Controls</label>
-
 			<hr>
-
 			<div class="input-button-container">
-
 				<div id="mute-button" class="button" onclick="mute();">Mute</div>
-
 				<div id="unmute-button" class="button" onclick="unmute();">Unmute</div>
-
 			</div>
 			
 			<br>
-
 		</div>
-
 		<div>
-
 			<label class="control-type">Loop Controls</label>
-
 			<hr>
-
 			<div class="input-button-container">
-
 				<div id="unloop-button" class="button" onclick="loop(false);">No Loop</div>
-
 				<div id="loop-button" class="button" onclick="loop(true);">Loop</div>
-
 			</div>
 			
 			<br>
-
 		</div>
-
 		<div>
-
 			<label class="control-type">Time Controls</label>
-
 			<hr>
-
 			<div class="input-button-container">
-
 				<input id="minute" class="input" placeholder="Minutes">
-
 				<input id="second" class="input" placeholder="Seconds">
-
 				<div id="time-button" class="button" onclick="doTime();">Jump To Time</div>
-
 			</div>
-
 			<br>
-
 		</div>
-
 		<div>
-
 			<label class="control-type">Volume Controls</label>
-
 			<hr>
-
 			<div class="input-button-container">
-
 				<input id="volume" class="input" placeholder="Volume">
-
 				<div id="volume-button" class="button" onclick="doSetVolume();">Set Volume</div>
-
 			</div>
 			
 			<br>
-
 		</div>
-
 		<div>
-
 			<label class="control-type">Speed Controls</label>
-
 			<hr>
-
 			<div class="input-button-container">
-
 				<input id="speed" class="input" placeholder="Playback Speed">
-
 				<div id="speed-button" class="button" onclick="doSetSpeed();">Set Speed</div>
-
 			</div>
 			
 			<br>
-
 		</div>
 	
 	`);
@@ -443,46 +366,31 @@ function controlPanel () {
 
 function helpPanel () {
 	$("#help-panel").html(`
-
 			<h1 class="title">How To Use The Player?</h1>
-
-
 			<p class="text">
 				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Welcome to the tutorial on how to use the player above. This tutorial will guide you through the process of displaying a video or playlist. It will also show you how to use the voice commands. As for the controls, they are pretty self-explanatory.
 			</p>
-
 			<br>
-
 			<h2 class="heading">Why Not Use The YouTube Website</h2>
-
       <p class="text">
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbspThe main question some of you may be asking is, why not just use the YouTube website? That is a very good question and my answer is that the YouTube website theme is mostly black and white. There is no color and no inspiration. It was meant to serve its purpose as an online video sharing site and it has served its purpose well. But there is almost no creativity in its design.
       </p>
-
       <br>
-
       <h2 class="heading">Getting An Id</h2>
-
 			<p class="text">
 				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Now, to the part you came for. The player works mostly by using the YouTube API and the YouTube Iframe API. These are two different APIs that allow this project to function properly. However these APIs like to work with the IDs of a video or playlist, not the actual link. So I included a link converter. Paste in the link and the converter will turn it into an ID.
 			</p>
-
 			<br>
-
 			<h2 class="heading">Loading A Video Or Playlist</h2>
-
 			<p class="text">
 				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;After you put an ID into the input box, simply press Submit ID and watch the magic happen. The video will turn into your chosen video and if it is a playlist then the playlist will fill in the other videos. Please note that the playlist can only hold 50 videos because the YouTube API Request will only return the last 50 videos in the playlist.
 			</p>
 		
 			<br>
-
 			<h2 class="heading">Using Voice Commands</h2>
-
 			<p class="text">
 				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Yes, there are voice commands in this website. See, how much work I put into it? Say Reset to reset the video and playlist. Say Next to play the next video in the playlist. Say Previous to play the previous video in the playlist. Say Play to play the video. Say Pause to pause the video. Say Stop to stop the video. Say Loop On to turn loop on. Say Loop Off to turn loop off. Say Mute to mute the video. Say Unmute to unmute the video.
 			</p>
-
 	`);
 }
 
